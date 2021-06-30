a7822e0f64fc2735233f1878f4066a6e5cf4ad9a
TebohoMosakoa <mosakoateboho@gmail.com>
2021-06-30 12:40:14 +0200

    Bet.Domain layer created

---
+++ src/Services/Bet/Bet.Domain/Common/EntityBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Bet.Domain.Common
+{
+    public abstract class EntityBase
+    {
+        public int Id { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string LastModifiedBy { get; set; }
+        public DateTime LateModifiedDate { get; set; }
+    }
+}
